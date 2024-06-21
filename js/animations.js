2db52e20eb4d82d500bfa93e7cc8d99d0c10f280
dacunc <clemendil@gmail.com>
2024-06-21 17:08:18 +0200

    Ajout de logique du carousel

--- js/animations.js
+++ js/animations.js
@@ -39,7 +39,6 @@ let tl1 = gsap.timeline({
         start: "top center",
         end: "top+=450 center",
         scrub: true,
-        // markers: true
     }
 });
 
@@ -49,16 +48,63 @@ let tl2 = gsap.timeline({
         start: "top+=700 center",
         end: "top+=1300 center",
         scrub: true,
-        // markers: true
     }
 });
 
-// Timeline pour about-caption-1
 tl1.fromTo("#about-caption-1", { opacity: 0, letterSpacing: 'normal' }, { opacity: 1, letterSpacing: '5px', duration: 0.5 })
     .to("#about-caption-1", { opacity: 0, duration: 0.5 });
 
-// Timeline pour about-caption-2 pour apparaître
 tl2.fromTo("#about-caption-2", { opacity: 0, scale: 1 }, { opacity: 1, scale: 1, duration: 0.5 })
     .to("#about-caption-2", { opacity: 1, scale: 1 })  // Maintient l'état pendant le scroll
     // Animation pour faire disparaître en s'éloignant
     .fromTo("#about-caption-2", { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5, duration: 0.5 });
+
+
+
+
+function customCursor() {
+    if (window.innerWidth > 1024) {
+        document.body.classList.add('desktop-theme');
+
+        const customCursor = document.createElement('div');
+        customCursor.classList.add('custom-cursor');
+        document.body.appendChild(customCursor);
+
+        // Met à jour la position du curseur
+        document.addEventListener('mousemove', (e) => {
+            gsap.to(customCursor, {
+                x: e.clientX,
+                y: e.clientY,
+                duration: 0.1,
+                ease: 'power2.out'
+            });
+        });
+
+        // Ajoute des effets de survol pour les liens
+        document.querySelectorAll('a, button, .carousel-item, .carousel-action:not(.disabled), .product-details-close-button').forEach((element) => {
+            element.addEventListener('mouseenter', () => {
+                gsap.to(customCursor, {
+                    width: '40px',
+                    height: '40px',
+                    duration: 0.2,
+                    ease: 'power2.out'
+                });
+            });
+
+            element.addEventListener('mouseleave', () => {
+                gsap.to(customCursor, {
+                    width: '20px',
+                    height: '20px',
+                    duration: 0.2,
+                    ease: 'power2.out'
+                });
+            });
+        });
+
+        hasCursor = true;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    customCursor();
+});
